f1cbc94f1308a186d89435da35a1a800f8f391cc
JorgePadilla <jorgep4dill4@gmail.com>
2021-10-21 19:28:20 -0600

    refactor post

--- js-built-in-fetch.js
+++ js-built-in-fetch.js
@@ -1,125 +1,21 @@
-/*function getMethod() {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}*/
-
-/*function getMethod() {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}*/
-//GET
-/*function testG() {
-  getData("https://jsonplaceholder.typicode.com/posts").then((data) => {
-    document.getElementById("get").innerHTML = JSON.stringify(data);
-  });
-}
-
-async function getData(url) {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-    mode: "cors",
-  });
-  return response.json();
-}
-
-function getMethod() {
-  getData("https://jsonplaceholder.typicode.com/posts").then((data) => {
-    console.log(data);
-  });
-}*/
-
-//POST
-
 function testP() {
-  var data = JSON.stringify({
-    title: "foo",
-    body: "bar",
-    userId: 1,
-  });
-  postData("https://jsonplaceholder.typicode.com/posts", data).then((data) => {
-    document.getElementById("post").innerHTML = JSON.stringify(data);
-  });
+  postData("https://jsonplaceholder.typicode.com/posts", post)
 }
 
-function postData(url, data) {
-  const response = await fetch(url, {
+var post = {
+  title: "foo",
+  body: "bar",
+  userId: 1,
+};
+
+function postData(url, post) {
+  fetch(url, {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      'Content-type': 'application/json; charset=UTF-8',
     },
-    body: JSON.stringify(data),
-  });
-  return response.json();
+    body: JSON.stringify(post),
+  }).then((response) => response.json())
+  .then((json) =>  document.getElementById("post").innerHTML = JSON.stringify(json));
 }
 
-/*function postMethod() {
-  postData("https://jsonplaceholder.typicode.com/posts", {
-    id: 101,
-    petId: 5,
-    quantity: 0,
-    shipDate: "2021-07-23T01:44:32.945Z",
-    status: "placed",
-    complete: false,
-  }).then((data) => {
-    console.log(data);
-  });
-}*/
-
-//PUT
-function testPu() {
-  getData("https://jsonplaceholder.typicode.com/posts/1").then((data) => {
-    document.getElementById("put").innerHTML = JSON.stringify(data);
-  });
-}
-
-async function postData(url = "", data = {}) {
-  const response = await fetch(url, {
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
-}
-
-function putMethod() {
-  postData("https://jsonplaceholder.typicode.com/posts/1", {
-    id: 1,
-    petId: 5,
-    quantity: 0,
-    shipDate: "2021-07-23T01:44:32.945Z",
-    status: "placed",
-    complete: false,
-  }).then((data) => {
-    console.log(data);
-  });
-}
-
-//DELETE
-function testD() {
-  getData("https://jsonplaceholder.typicode.com/posts/1").then((data) => {
-    document.getElementById("delete").innerHTML = JSON.stringify(data);
-  });
-}
-testD();
-
-async function postData(url = "", data = {}) {
-  const response = await fetch(url, {
-    method: "DELETE",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  return response.json();
-}
-
-function deleteMethod() {
-  postData("https://jsonplaceholder.typicode.com/posts/1", {}).then((data) => {
-    console.log(data);
-  });
-}
